0e72bc9b39014b338fbceccecc961fcd0def06a8
Vitlaiy Mikhailov <vitaliy-mikhailov@yandex.ru>
2023-10-26 00:07:19 +0300

    debug install_ruby.sh 2

--- install_ruby.sh
+++ install_ruby.sh
@@ -2,10 +2,5 @@
 sudo apt update
 sudo apt install -y ruby-full ruby-bundler build-essential
 
-CHECK_RUBY=`apt list --installed |grep ruby`
-CHECK_BUNDLER=`apt list --installed |grep bundler`
-
-if [ -n "$CHECK_RUBY"  ] && [ -n "$CHECK_BUNDLER"  ]; then
-echo "Ruby and Bundler installed"
-else echo "Ruby or Bundler not installed"
-fi
+ruby -v && echo "---Ruby installed---"
+bundler -v  && echo "---Bundler installed---"
